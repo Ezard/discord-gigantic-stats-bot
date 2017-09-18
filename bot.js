1d1cef656fbf5bffdcb422d03ca633710671178d
Ezard <ezard.ben@gmail.com>
2017-09-18 18:05:29 +0100

    Allow multiple users to be queried

--- bot.js
+++ bot.js
@@ -7,29 +7,32 @@ const token = 'MzU5MDU0MTA4Njg0MTg5Njk3.DKByhw.TTrBp5F_D6bNWfY0NXPQE2TQ0sE';
 
 client.on('message', message => {
 	if (message.content.startsWith('!stat ')) {
-		let user = message.content.split(' ')[1];
-		let userEncoded = user.replace('#', '%23');
-		http.get({
-			host: 'gigantic-mmr-api.azurewebsites.net',
-			path: '/leaderboards/' + userEncoded
-		}, res => {
-			let rawData = '';
-			res.on('data', chunk => rawData += chunk);
-			res.on('end', () => {
-				let json = JSON.parse(rawData);
-				let data = json.data[user];
+		let split = message.content.split(' ');
+		for (let i = 1; i < split.length; i++) {
+			let user = split[i];
+			let userEncoded = user.replace('#', '%23');
+			http.get({
+				host: 'gigantic-mmr-api.azurewebsites.net',
+				path: '/leaderboards/' + userEncoded
+			}, res => {
+				let rawData = '';
+				res.on('data', chunk => rawData += chunk);
+				res.on('end', () => {
+					let json = JSON.parse(rawData);
+					let data = json.data[user];
 
-				const embed = new Discord.RichEmbed();
-				embed.setTitle(user);
-				embed.addField('MMR', Math.round(data.all.total.motiga_skill * 100), true);
-				embed.addField('Level', data.all.total.rank, true);
-				embed.addField('Win Rate', Math.round((data.all.total.wins / data.all.total.total_games) * 100) + '%', true);
-				embed.addField('Games Played', data.all.total.total_games, true);
-				embed.addField('Hours Played', Math.round(data.all.total.time_played / 3600), true);
-				embed.addField('Most Played Hero', getMostPlayedHero(data), true);
-				message.channel.send(embed);
+					const embed = new Discord.RichEmbed();
+					embed.setTitle(user);
+					embed.addField('MMR', Math.round(data.all.total.motiga_skill * 100), true);
+					embed.addField('Level', data.all.total.rank, true);
+					embed.addField('Win Rate', Math.round((data.all.total.wins / data.all.total.total_games) * 100) + '%', true);
+					embed.addField('Games Played', data.all.total.total_games, true);
+					embed.addField('Hours Played', Math.round(data.all.total.time_played / 3600), true);
+					embed.addField('Most Played Hero', getMostPlayedHero(data), true);
+					message.channel.send(embed);
+				});
 			});
-		});
+		}
 	}
 });
 
